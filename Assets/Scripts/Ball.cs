932859a51743723699179662cc7451823c55c49d
James Edens <jedensuscg@gmail.com>
2020-05-22 16:23:32 -0400

    Added some sounds when ball hits things.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,6 +10,7 @@ public class Ball : MonoBehaviour
     [SerializeField] float xPushMax;
     [SerializeField] float yPushMin;
     [SerializeField] float yPushMax;
+    [SerializeField] AudioClip[] ballSounds;
 
     // state
     Vector2 ballToPaddleVector;
@@ -17,11 +18,15 @@ public class Ball : MonoBehaviour
     private float xPush;
     private float yPush;
 
+    // Cached component references
+    AudioSource myAudioSource;
+
 
     // Start is called before the first frame update
     void Start()
     {
         ballToPaddleVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -53,4 +58,15 @@ public class Ball : MonoBehaviour
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + ballToPaddleVector;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasLaunched)
+        {
+            AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
+        
+    }
 }
+ 
